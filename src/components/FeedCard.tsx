de16ef0137ed4baf00e3c67c6765db074ab29d9a
MohamedRoshanPU <roshanparambil7@gmail.com>
2024-10-01 08:55:29 +0530

    removed dummy data

--- src/components/FeedCard.tsx
+++ src/components/FeedCard.tsx
@@ -10,7 +10,6 @@ const FeedCard: React.FC<FeedCardProps> = ({ data }) => {
       <img src={data?.node?.field_photo_image_section} alt="" />
       <div className="feed-card-details">
         <h2>{data?.node?.title}</h2>
-        <p>Dec 2019 , 2021 3:32AM IST</p>
       </div>
     </div>
   );
